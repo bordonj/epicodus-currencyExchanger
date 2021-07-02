d17efe5a5d867feb62d77df262e40a716960505d
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-02 08:18:11 -0700

    add the hidden key

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 // import { Project } from 'js/project';
+
+console.log('key', process.env.API_KEY);
