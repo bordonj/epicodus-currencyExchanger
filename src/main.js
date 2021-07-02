7ff81095f9c4d8acb580719d00ceb187e205d93b
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-02 08:38:26 -0700

    add note for development

--- src/main.js
+++ src/main.js
@@ -5,3 +5,15 @@ import './css/styles.css';
 // import { Project } from 'js/project';
 
 console.log('key', process.env.API_KEY);
+
+
+/* NOTES FOR DEV */
+// https://v6.exchangerate-api.com/v6/bb0b3db76944e54e64a43d98/pair/EUR/GBP
+
+// PHP	Philippine Peso	Philippines
+// THB	Thai Baht	Thailand
+// TWD	New Taiwan Dollar	Taiwan
+// VND	Vietnamese Đồng	Vietnam
+// SGD	Singapore Dollar	Singapore
+// JPY	Japanese Yen	Japan
+// KRW	South Korean Won	South Korea
