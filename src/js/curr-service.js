a089c0e4e19fa0b3b8a3cc90c2a8afb81c6c0ce4
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-02 09:06:30 -0700

    add logic for currency class

--- src/js/curr-service.js
+++ src/js/curr-service.js
@@ -1,6 +1,23 @@
+import axios from 'axios';
+
+export default class Currency {
+  static async convert(firstCurr, secondCurr) {
+    try {
+      let response = axios.get(`https://v6.exchangerate-api.com/v6/bb0b3db76944e54e64a43d98/pair/${firstCurr}/${secondCurr}`);
+      console.log('axios get', response);
+      return (await response).data;
+    }
+    catch (err) {
+      console.log('err', err);
+      return err;
+    }
+  }
+}
+
+
 // export function Project() {
 // }
 
 // example commits for the following:
-  // Good Fail: Add first test and Example Class.
-  // Passing: Add constructor logic, pass first test.
+// Good Fail: Add first test and Example Class.
+// Passing: Add constructor logic, pass first test.
