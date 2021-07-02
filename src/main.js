e49f7a39d7b9680655227b6d2cb1fd3206f76bb7
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-02 13:46:07 -0700

    add logic for changing first input

--- src/main.js
+++ src/main.js
@@ -4,7 +4,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Currency from './js/curr-service';
 
+let grabElements = () => {
+  let amt = parseFloat($('#mine').val());
+  let amt2 = parseFloat($('#mine').val());
+  let firstInput = $('#dropDown1').val();
+  console.log('firstInput', firstInput);
+  let secondInput = $('#dropDown2').val();
+  console.log('secondInput', secondInput);
+  return [firstInput, secondInput, amt, amt2];
+};
+
 $(document).ready(() => {
+  $('#mine').on('change', () =>{
+    if ($('#mine').val() === '0') {
+      $('#want').attr("value", `0`);
+    } else {
+      let elArr = grabElements();
+      Currency.convert(elArr[0], elArr[1], elArr[2])
+        .then(res => {
+          console.log('res', res);
+          $('#want').attr("value", `${res.conversion_result}`);
+          $('.conversion').html(`
+            <p>${elArr[2]} ${elArr[0]} equals</p>
+            <h2>${elArr[1]} ${res.conversion_result}</h2>
+            <p>converstion rate: ${res.conversion_rate}</p>
+          `);
+        });
+    }
+  });
+
+
+
+
+
+
+
+
+
+
+
+  //below used for checking logic
   let amt = parseFloat($('#inputAmt').val());
   console.log('amt input', amt);
   $('#currInput').on('submit', (e) => {
@@ -58,6 +97,16 @@ $(document).ready(() => {
 });
 
 
+
+// TO CHANGE THE PLACEHOLDER VALUE
+// $(document).ready(function(){ 
+//   $('form').find("input[type=textarea], input[type=password], textarea").each(function(ev)
+//   {
+//     if(!$(this).val()) { 
+//       $(this).attr("placeholder", "Type your answer here");
+//   }
+//   });
+// });
 /* NOTES FOR DEV */
 // https://v6.exchangerate-api.com/v6/bb0b3db76944e54e64a43d98/pair/EUR/GBP
 
