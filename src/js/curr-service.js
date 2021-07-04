c98a60e50ebdb4b7ddb23d0d41b0a06f27a64457
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-04 08:40:21 -0700

    clean code

--- src/js/curr-service.js
+++ src/js/curr-service.js
@@ -11,11 +11,3 @@ export default class Currency {
     }
   }
 }
-
-
-// export function Project() {
-// }
-
-// example commits for the following:
-// Good Fail: Add first test and Example Class.
-// Passing: Add constructor logic, pass first test.
