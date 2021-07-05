797c8a572ca40753cfd63859606ed8c0a4754115
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-07-04 19:25:42 -0700

    update currency-service

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default class Currency {
   static async convert(firstCurr, secondCurr, amt) {
     try {
-      let response = axios.get(`https://v6.exchangerate-api.com/v6/bb0b3db76944e54e64a43d98/pair/${firstCurr}/${secondCurr}/${amt}`);
+      let response = axios.get(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${firstCurr}/${secondCurr}/${amt}`);
       return (await response).data;
     }
     catch (err) {
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Currency from './js/curr-service';
+import Currency from './js/currency-service';
 
 const addCommas = numStr => {
   let counter = 0;
