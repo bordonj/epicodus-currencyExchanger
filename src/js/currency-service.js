9e622613505002a6e55d0274ec149bfe8f1c744e
Jennifer Bordon <80655182+bordonj@users.noreply.github.com>
2021-09-21 13:43:16 -0700

    update to use local storage

--- src/js/currency-service.js
+++ src/js/currency-service.js
@@ -2,12 +2,19 @@ import axios from 'axios';
 
 export default class Currency {
   static async convert(firstCurr, secondCurr, amt) {
-    try {
-      let response = axios.get(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${firstCurr}/${secondCurr}/${amt}`);
-      return (await response).data;
-    }
-    catch (err) {
-      return err;
+    const conversionRate = window.localStorage.getItem(`${firstCurr} - ${secondCurr}`);
+    if (conversionRate) {
+      const result = parseFloat(amt) * parseFloat(conversionRate);
+      return {conversion_result: result, conversion_rate: conversionRate};
+    } else {
+      try {
+        let response = await axios.get(`https://v6.exchangerate-api.com/v6/bb0b3db76944e54e64a43d98/pair/${firstCurr}/${secondCurr}/${amt}`);
+        window.localStorage.setItem(`${firstCurr} - ${secondCurr}`, response.data.conversion_rate);
+        return response.data;
+      }
+      catch (err) {
+        return err;
+      }
     }
   }
 }
